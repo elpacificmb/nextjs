7459bd0acb83fbd61edff838132d081b9fe4b1cd
elpacificmb <elpacificmb@gmail.com>
2021-08-26 04:06:51 +0200

    next basics

---
+++ README.OLD.md
@@ -0,0 +1,24 @@
+# nextjs
+
+Next js
+
+## Install Nextjs
+
+npx create-next-app nextjs --use-npm
+
+## Start the server
+
+npm dev start
+
+## Create static files and build
+
+"build": "next build && next export"
+npm run build
+
+## install serve
+
+npm i -g serve
+
+## serve static files
+
+serve -s out -p 8000
---
+++ components/Meta.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+const Meta = ({title, keywords, description}) => {
+  return (
+    <Head>
+      <meta name='viewport' content='width=device, initial-scale=1' />
+      <meta name='keywords' content={keywords} />
+      <meta name='description' content={description} />
+      <meta charSet='utf-8' />
+      <link rel='icon' href='/favicon.ico' />
+      <title>{title}</title>
+    </Head>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Web Dev Next',
+  keywords: 'web development, programming, nextjs',
+  description: 'Get the latest web dev news'
+}
+
+export default Meta
